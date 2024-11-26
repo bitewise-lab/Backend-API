a3a3b0c821d33ea13d8dcf7c36f55b4325bb448d
Wilson Boyaron Hutagalung <110729448+WilsonHutagalung@users.noreply.github.com>
2024-11-26 10:50:22 +0800

    Update server.js

--- backend-api-bitewise/server.js
+++ backend-api-bitewise/server.js
@@ -1,23 +1,31 @@
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors');
-const routes = require('./routes/mlRoutes');
-const authroutes = require('./routes/authRoutes')
+const mlroute = require('./routes/mlRoutes');
+const authroute = require('./routes/authRoutes');
 const bodyParser = require('body-parser');
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
+const host = process.env.HOST || 'localhost';
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/api/mlRoutes', routes);
-app.use('/api/auth', authroutes)
+
+app.use('/api', mlroute);   
+app.use('/api', authroute);
 
 app.get('/', (req, res) => {
     res.send('API is running');
 });
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    const url = `http://${host}:${port}`;
+    console.log(`Server running at ${url}`);
 });
