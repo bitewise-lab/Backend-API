35c3c3bd3b5b6d8ca81f5af6748aa3eef8e99a25
Wilson Boyaron Hutagalung <110729448+WilsonHutagalung@users.noreply.github.com>
2024-12-11 18:19:32 +0800

    Delete backend-api-bitewise directory

--- backend-api-bitewise/routes/mlRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const mlroute = express.Router();
-const { getPredictionHandler } = require('../controllers/mlController');
-
-mlroute.post('/predict', getPredictionHandler);
-
-module.exports = mlroute;
